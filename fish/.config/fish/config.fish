2dc3b890690fd77e494b8907598edb1e9e65aae3
sean <sean.p.reichel@gmail.com>
2025-05-10 19:16:33 -0400

    fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,8 +6,8 @@ if status is-interactive
   set EDITOR "/usr/bin/nvim"
   set STARSHIP_CONFIG "~/.config/starship/starship.toml"
 
-  direnv hook fish | source
-  starship init fish | source
+  #direnv hook fish | source
+  #starship init fish | source
 
   if test -f /usr/bin/eza
     alias ls="eza -1 -l --icons=auto -h -g -o --total-size --time-style 'relative' --group-directories-first --no-permissions"
